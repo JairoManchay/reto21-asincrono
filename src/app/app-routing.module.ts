9c749a04abcba7b01fc3a212110ec629421156ab
RodrigoProject <jairoRodrigoMP@gmail.com>
2023-01-22 23:34:57 -0500

    Subeidno la imagen restante

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProtectedGuard } from './protected.guard';
+import { VistaComponent } from './components/vista/vista.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'vista', component: VistaComponent, canActivate:[ProtectedGuard]},
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
